367ccc3d8e1f4de531eb477f8317d3a0f1751c7d
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-18 15:32:37 +0700

    insert data

--- repository/url_repositoryImp.go
+++ repository/url_repositoryImp.go
@@ -11,5 +11,14 @@ type urlRepositoryImp struct {
 }
 
 func (repository *urlRepositoryImp) save(ctx context.Context, url domain.URL) (domain.URL, error) {
-
+	script := "INSERT INTO urls (code,long_url,hit_count) VALUES ?,?,?"
+	result, err := repository.DB.ExecContext(ctx, script, url.Code, url.LongURL, url.HitCount)
+	if err != nil {
+		return url, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		url.Id = id
+	}
+	return url, nil
 }
