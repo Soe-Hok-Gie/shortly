699ed67cd5521240f3519732cb69263393fd41cf
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-19 16:29:29 +0700

    modifikasi dan membuat func polimerism

--- service/url_serviceImp.go
+++ service/url_serviceImp.go
@@ -10,12 +10,17 @@ type urlServiceImp struct {
 	UrlRepository repository.UrlRepository
 }
 
-func (s *urlServiceImp) Save(ctx context.Context, longURL string) (domain.URL, error) {
+func NewUrlService(urlRepository repository.UrlRepository) UrlService {
+	return &urlServiceImp{UrlRepository: urlRepository}
+}
+
+func (service *urlServiceImp) Save(ctx context.Context, longURL string) (domain.URL, error) {
 	url := domain.URL{
 		Code:     "abc123", // nanti bisa diganti random generator
 		LongURL:  longURL,
 		HitCount: 0,
 	}
 
-	return s.UrlRepository.Save(ctx, url)
+	return service.UrlRepository.Save(ctx, url)
+
 }
