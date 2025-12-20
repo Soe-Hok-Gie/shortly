932ddebc4f500f3fd429b1183c7cfd2f69272ab5
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-20 13:34:11 +0700

    modifikasi dgn menambahkan func generator

--- service/url_serviceImp.go
+++ service/url_serviceImp.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"math/rand"
 	"shortly/model/domain"
 	"shortly/repository"
+	"time"
 )
 
 type urlServiceImp struct {
@@ -14,13 +16,29 @@ func NewUrlService(urlRepository repository.UrlRepository) UrlService {
 	return &urlServiceImp{UrlRepository: urlRepository}
 }
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func (service *urlServiceImp) Save(ctx context.Context, longURL string) (domain.URL, error) {
 	url := domain.URL{
-		Code:     "abc123", // nanti bisa diganti random generator
+		Code:     generateShortCode(6),
 		LongURL:  longURL,
 		HitCount: 0,
 	}
 
 	return service.UrlRepository.Save(ctx, url)
 
+}
+
+func generateShortCode(length int) string {
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	result := make([]byte, length)
+
+	for i := 0; i < length; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+
+	return string(result)
 }
