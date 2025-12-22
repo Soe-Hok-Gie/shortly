ea1ae2a46dfec0b74a1fd48ed36f0bd888b7f7da
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-22 13:28:02 +0700

    membuat endpoint findbyshortcode repo

--- repository/url_repositoryImp.go
+++ repository/url_repositoryImp.go
@@ -30,3 +30,21 @@ func (repository *urlRepositoryImp) Save(ctx context.Context, url domain.URL) (d
 	}
 	return url, nil
 }
+
+func (repository *urlRepositoryImp) FindByShortCode(ctx context.Context, code string) (domain.URL, error) {
+	script := "SELECT id, code,long_url from urls WHERE code =?"
+	row := repository.DB.QueryRowContext(ctx, script, code)
+
+	var url domain.URL
+	err := row.Scan(
+		url.Id,
+		url.Code,
+		url.LongURL,
+		url.HitCount,
+	)
+	if err != nil {
+		fmt.Println("err", err)
+		return url, err
+	}
+	return url, nil
+}
