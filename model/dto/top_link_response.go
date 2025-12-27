a88fe415cf398afabf7ba1f3c4f07a37d35645c0
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-27 16:12:03 +0700

    delete field short_code

--- model/dto/top_link_response.go
+++ model/dto/top_link_response.go
@@ -2,7 +2,6 @@ package dto
 
 type TopLinkResponse struct {
 	Code     string `json:"code"`
-	ShortURL string `json:"short_url"`
 	LongURL  string `json:"long_url"`
 	HitCount int64  `json:"hit_count"`
 }
