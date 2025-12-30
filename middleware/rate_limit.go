48f7456e707b5049063b32d3ca5d12c20aef2bbb
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-30 15:14:45 +0700

    membuat struct ratelimitdata

--- middleware/rate_limit.go
+++ middleware/rate_limit.go
@@ -3,10 +3,16 @@ package middleware
 import (
 	"net/http"
 	"sync"
+	"time"
 )
 
 type RateLimitMiddleware struct {
 	Handler      http.Handler
 	RateLimitMap map[string]*RateLimitData
 	Mutex        *sync.Mutex
+}
+
+type RateLimitData struct {
+	HitCount int
+	FirstHit time.Time
 }
