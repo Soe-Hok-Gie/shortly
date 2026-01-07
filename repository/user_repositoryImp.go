2cf66b2c63d2162e3c3df3e9fc5b092ee756c143
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-01-07 17:03:58 +0700

    implementasi query. danfeat(user-repo): add func to detect duplicate username error

--- repository/user_repositoryImp.go
+++ repository/user_repositoryImp.go
@@ -3,11 +3,39 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"shortly/model/domain"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 type userRepositoryImp struct {
 	DB *sql.DB
 }
 
-func (repository *userRepositoryImp) Save(ctx context.Context, user domain.User) (error, domain.User)
+func (repository *userRepositoryImp) Save(ctx context.Context, user domain.User) (domain.User, error) {
+	script := "INSERT INTO users (username,password) VALUES (?,?)"
+	result, err := repository.DB.ExecContext(ctx, script, user.Username, user.Password)
+	if err != nil {
+		if isDuplicateKeyError(err) {
+			return user, errors.New("username already exists")
+		}
+		return user, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return user, err
+	}
+
+	user.Id = id
+	return user, nil
+}
+
+// Helper untuk cek duplicate key MySQL
+func isDuplicateKeyError(err error) bool {
+	if me, ok := err.(*mysql.MySQLError); ok {
+		return me.Number == 1062
+	}
+	return false
+}
