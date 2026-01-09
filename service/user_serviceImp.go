23dfc2aa20aea0400f4f79fd58c60844dcf1fca6
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-01-09 15:39:21 +0700

    Validasi inputan

--- service/user_serviceImp.go
+++ service/user_serviceImp.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"shortly/model/domain"
 	"shortly/model/dto"
 	"shortly/repository"
@@ -12,5 +13,9 @@ type userServiceImp struct {
 }
 
 func (service *userServiceImp) Save(ctx context.Context, input dto.CreateUserInput) (domain.User, error) {
+
+	if input.Username == "" || input.Password == "" {
+		return domain.User{}, errors.New("Username & Password not requaired")
+	}
 
 }
