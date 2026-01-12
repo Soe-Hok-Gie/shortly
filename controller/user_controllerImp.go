c9eae86e1620c875802158ae2ba1cc2fea90b97a
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-01-12 15:36:57 +0700

    implementasi user-register-handler : cek error duplikat username

--- controller/user_controllerImp.go
+++ controller/user_controllerImp.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
+	"shortly/model/dto"
 	"shortly/service"
 )
 
@@ -9,6 +11,46 @@ type userControllerImp struct {
 	userService service.UserService
 }
 
-func (controller *userControllerImp) Save(writer http.ResponseWriter, request *http.Request) {
+func NewUserController(userService service.UserService) userController {
+	return &userControllerImp{userService: userService}
+}
 
+func (controller *userControllerImp) Save(writer http.ResponseWriter, request *http.Request) {
+	ctx := request.Context()
+
+	var req dto.CreateUserInput
+	if err := json.NewDecoder(request.Body).Decode(&req); err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(writer).Encode(dto.Response{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   "Input salah",
+		})
+		return
+	}
+
+	result, err := controller.userService.Save(ctx, req)
+	if err != nil {
+		status := http.StatusBadRequest
+		if err.Error() == "username already exists" {
+			status = http.StatusConflict
+		}
+
+		writer.WriteHeader(status)
+		json.NewEncoder(writer).Encode(dto.Response{
+			Code:   status,
+			Status: http.StatusText(status),
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	json.NewEncoder(writer).Encode(dto.Response{
+		Code:   http.StatusCreated,
+		Status: "Register Sukses",
+		Data:   result,
+	})
 }
