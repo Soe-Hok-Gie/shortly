a2ab91fd902914fee4843e95f17ad77d2138a9f6
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-01-28 17:09:10 +0700

    membuat struct dto LoginResponse untuk login jwt

--- model/dto/user_create_response.go
+++ model/dto/user_create_response.go
@@ -6,3 +6,8 @@ type UserResponse struct {
 	Username   string    `json:"username"`
 	Created_At time.Time `json:"created_at"`
 }
+
+type LoginResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"` // opsional, biasanya "JWT"
+}
