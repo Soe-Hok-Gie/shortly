3863dea7e5c5106e773e03dc53c75235a61222a6
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-01-28 17:11:58 +0700

    implement login JWT token generate

---
+++ utils/jwt.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(Id int64, username string) (string, error) {
+	fmt.Println("DEBUG user.Id:", Id)
+
+	secret := []byte(os.Getenv("JWT_SECRET"))
+	if len(secret) == 0 {
+		return "", errors.New("JWT_SECRET is empty")
+	}
+
+	now := time.Now()
+	claims := jwt.RegisteredClaims{
+		Subject:   strconv.FormatInt(Id, 10),
+		IssuedAt:  jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(now.Add(15 * time.Minute)),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secret)
+}
