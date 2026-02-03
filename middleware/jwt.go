00bc688c5bdd4c8afb5b10efa4c887093001dd54
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-02-03 14:53:55 +0700

    membuat context type userid dan func jwt middleware

---
+++ middleware/jwt.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "net/http"
+
+type contextKey string
+
+const UserIdKey contextKey = "Id"
+
+func JWTMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+
+	})
+}
