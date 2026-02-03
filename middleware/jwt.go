48f4c46765ef11863bf89380460332eab3b4a9d1
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-02-03 15:00:29 +0700

    ambil authHeader

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -9,5 +9,11 @@ const UserIdKey contextKey = "Id"
 func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 
+		authHeader := request.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(writer, "authorization header missing", http.StatusUnauthorized)
+			return
+		}
+
 	})
 }
