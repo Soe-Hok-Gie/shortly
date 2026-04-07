d5825a625eb7f3f9d8614245608c142425af6a15
Soe-Hok-Gie <fadilspiegel@gmail.com>
2026-04-07 15:28:05 +0700

    create func generaterefreshtoken,validaterefreshtoken and hashrefresh

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"os"
@@ -27,4 +29,65 @@ func GenerateToken(Id int64) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secret)
+}
+
+func GenerateRefreshToken(Id int64) (string, error) {
+	secret := []byte(os.Getenv("JWT_REFRESH"))
+	if len(secret) == 0 {
+		return "", errors.New("JWT_REFRESH is EMPTY")
+	}
+	now := time.Now()
+	claims := jwt.RegisteredClaims{
+		Issuer:    "service refresh token",
+		Subject:   strconv.FormatInt(Id, 10),
+		IssuedAt:  jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(now.Add(7 * 24 * time.Hour)),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secret)
+}
+
+// tambahkan Validate Token
+func ValidateRefreshToken(tokenString string) (*int64, error) {
+	secret := os.Getenv("REFRESH_SECRET")
+	if secret == "" {
+		return nil, errors.New("REFRESH_SECRET is empty")
+	}
+
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&jwt.RegisteredClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+
+			return []byte(os.Getenv("REFRESH_SECRET")), nil
+		},
+	)
+
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid refresh token")
+	}
+
+	Id, err := strconv.ParseInt(claims.Subject, 10, 64)
+	if err != nil {
+		return nil, errors.New("invalid subject in refresh token")
+	}
+
+	return &Id, nil
+
+}
+
+func Hashrefresh(token string) string {
+	hash := sha256.Sum256([]byte(token))
+	return hex.EncodeToString(hash[:])
+
 }
